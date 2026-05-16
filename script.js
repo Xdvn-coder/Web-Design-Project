b71720bb6aaafe3f8d555683fe1001535cde80e5
Davan <dafanmuhammad0@gmail.com>
2026-05-16 16:35:46 +0700

    Memperbarui tampilan kontak

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const menu = document.getElementById("nav");
     hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+//script kotak saran 
+function kirimPesan(event) {
+    event.preventDefault();
+    alert("Terima kasih, saran, Keluhan atau Komentar Anda telah kami terima!");
+    window.location.href = "kontak.html";
+}
