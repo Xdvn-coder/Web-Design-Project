e5845411e06f69c2df6a7a1e0f5a88a62f38b66a
Davan <dafanmuhammad0@gmail.com>
2026-05-29 17:52:22 +0700

    Menambahkan pop up pada produk dan menambahkan fitur sliders di halaman utama

--- script.js
+++ script.js
@@ -10,3 +10,55 @@ function kirimPesan(event) {
     alert("Terima kasih, saran, Keluhan atau Komentar Anda telah kami terima!");
     window.location.href = "kontak.html";
 }
+
+//script pop up detail produkk
+const modal = document.getElementById("openproduk");
+
+    function openProduk(image, title, price, kontak, logo){
+        openproduk.classList.add("active");
+        openproduk.classList.remove("hide");
+        openproduk.style.display = "flex";
+        document.getElementById("produk-img").src = image;
+        document.getElementById("produk-name").innerText = title;
+        document.getElementById("produk-price").innerText = price;
+        document.getElementById("produk-kontak").innerHTML = kontak;
+    }
+
+    function closeProduk(){
+        openproduk.classList.add("hide");
+        setTimeout(() => {
+            modal.classList.remove("active");
+        }, 300);
+    }
+
+    window.onclick = function(e){
+        if(e.target == openproduk){
+            closeProduk();
+        }
+    }
+
+//script slider
+const slides = document.getElementById("slides");
+let index = 0;
+const totalSlides = 2;
+
+    function showSlide(){
+        slides.style.transform = `translateX(-${index * 400}px)`;
+    }
+    function nextSlide(){
+        index++;
+        if(index > totalSlides){
+            index = 0;
+        }
+        showSlide();
+    }
+    function prevSlide(){
+        index--;
+        if(index < 0){
+            index = totalSlides - 1;
+        }
+        showSlide();
+    }
+
+    // otomatis geser tiap 3 detik
+    setInterval(nextSlide, 3000);
